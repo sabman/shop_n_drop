3b7b5dd4d1062b74fb2a6e22d1f7351342c051f4
sabman <saburq@gmail.com>
2013-05-22 14:08:56 +0200

    Initial commit - rails 4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShopNDrop::Application.config.secret_key_base = '68aa8b73534c80047f4480c9ba12fef8997ce6be5648e5f89b8d219fcac88466c6058f7aa97bcb6722a3ab7c369eeb585b0427a401ef7d9231247898c71d252f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ShopNDrop::Application.config.session_store :cookie_store, key: '_shop_n_drop_session'
